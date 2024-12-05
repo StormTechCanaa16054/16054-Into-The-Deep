0bc649ee7f1911382ea831123efd3c7feeae80d4
StormTech Canaã <stormtechcanaa16054@gmail.com>
2024-12-05 15:33:47 -0300

    Update TestePID.java

--- src/main/java/org/firstinspires/ftc/teamcode/PID/TestePID.java
+++ src/main/java/org/firstinspires/ftc/teamcode/PID/TestePID.java
@@ -16,7 +16,7 @@ public class TestePID extends LinearOpMode {
 
     @Override
     public void runOpMode() {
-        // Inicialização do hardware
+    
         slideH = hardwareMap.get(DcMotor.class, "slideH");
         slideH.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         slideH.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
